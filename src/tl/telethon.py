e51eff6b0dbc561ed9a7402f72dc2dd2a3987c9f
Sergey Shadrin <66666923+SHADR1N@users.noreply.github.com>
2024-07-03 07:58:04 +0300

    Update telethon.py
    zzz

--- src/tl/telethon.py
+++ src/tl/telethon.py
@@ -555,7 +555,7 @@ class TelegramClient(telethon.TelegramClient, BaseObject):
         **kwargs,
     ) -> TelegramClient:
 
-        newClient = TelegramClient(session, api=api, password=password, **kwargs)
+        newClient = TelegramClient(session, api=api, **kwargs)
 
         try:
             await newClient.connect()
